4b4f204502e958f7ba052ebab338a0da54020c4a
Graham Turk <gturk9@gmail.com>
2015-07-05 22:46:13 -0400

    Added logic for navbar link highlighting and fixed twitter feed

--- script-main.js
+++ script-main.js
@@ -21,7 +21,56 @@ $(document).ready(function() {
 		event.preventDefault();
 		$('#main-nav').toggleClass("open");
 	});
+
+	$(window).scroll(function(){
+        var window_top = $(window).scrollTop() + 70; // the "12" should equal the margin-top value for nav.stick
+        var div_top = $('#twitter-anchor').offset().top;
+            if (window_top > div_top) {
+                $('#twitter-inside').addClass('stick');
+            } else {
+                $('#twitter-inside').removeClass('stick');
+            }
+    });
+
+	/**
+     * This part handles the highlighting functionality.
+     * We use the scroll functionality again, some array creation and 
+     * manipulation, class adding and class removing, and conditional testing
+     */
+    var aChildren = $("#main-nav li").children(); // find the a children of the list items
+    var aArray = []; // create the empty aArray
+    for (var i=0; i < aChildren.length; i++) {    
+        var aChild = aChildren[i];
+        var ahref = $(aChild).attr('href');
+        aArray.push(ahref);
+    } // this for loop fills the aArray with attribute href values
+
+    $(window).scroll(function(){
+        var windowPos = $(window).scrollTop(); // get the offset of the window from the top of page
+        var windowHeight = $(window).height(); // get the height of the window
+        var docHeight = $(document).height();
+
+        for (var i=0; i < aArray.length; i++) {
+            var theID = aArray[i];
+            var divPos = $(theID).offset().top; // get the offset of the div from the top of page
+            var divHeight = $(theID).height(); // get the height of the div in question
+            if (windowPos >= divPos && windowPos < (divPos + divHeight)) {
+                $("a[href='" + theID + "']").addClass("nav-active");
+            } else {
+                $("a[href='" + theID + "']").removeClass("nav-active");
+            }
+        }
+
+        if(windowPos + windowHeight == docHeight) {
+            if (!$("#main-nav li:last-child a").hasClass("nav-active")) {
+                var navActiveCurrent = $(".nav-active").attr("href");
+                $("a[href='" + navActiveCurrent + "']").removeClass("nav-active");
+                $("#main-nav li:last-child a").addClass("nav-active");
+            }
+        }
+    });
 });
+
 // scroll function
 function scrollToID(id, speed){
 	var offSet = 50;
